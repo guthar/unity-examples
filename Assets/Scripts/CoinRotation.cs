ec84cc9e436dc1010d9211730c7a1acc5bdeac25
Markus Eßmayr <github@t-mx.com>
2019-09-30 15:31:46 +0200

    Prefabs für Münzen und Geldscheine hinzugefügt

---
+++ Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    /// <summary>
+    /// Stärke der Rotation.
+    /// </summary>
+    public float rotationSpeed = 50f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/NoteRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteRotation : MonoBehaviour
+{
+    /// <summary>
+    /// Stärke der Rotation.
+    /// </summary>
+    public float rotationSpeed = 50f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationSpeedAdjusted = rotationSpeed * Time.deltaTime;
+        transform.Rotate(rotationSpeedAdjusted, rotationSpeedAdjusted * 2, 0);
+    }
+}
