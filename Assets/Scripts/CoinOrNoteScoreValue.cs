ec71b33ef0199d202fb85ebd0ed6645048e9638f
Markus Eßmayr <github@t-mx.com>
2019-09-30 16:49:04 +0200

    Ton zum Einsammeln von Geld hinzugefügt. Berechnung des Punktewertes hinzugefügt.

---
+++ Assets/Scripts/CoinOrNoteScoreValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CoinOrNoteScoreValue : MonoBehaviour
+{
+    /// <summary>
+    /// Punktefaktor
+    /// </summary>
+    public int scoreFactor = 100;
+
+    /// <summary>
+    /// Gibt den Wert des Geldobjekts zurück.
+    /// </summary>
+    public int ScoreValue
+    {
+        get
+        {
+            int result = (int)(gameObject.transform.position.y * scoreFactor);
+            return result;
+        }
+    }
+}
