d2a5dbe9ba1a38eeb190849aa7fbeb64dbb9280c
Andreas Altenhofer <andreas.altenhofer@r-software.at>
2019-09-30 19:22:09 +0200

    Model Generator hinzugefügt

---
+++ Assets/Areal/CityLoader.cs
@@ -0,0 +1,70 @@
+﻿using Dummiesman;
+using GeoJSON;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CityLoader : MonoBehaviour
+{
+    public TextAsset encodedGeoJSON;
+
+    public GeoJSON.FeatureCollection collection;
+
+    private const string AREAL_PATH = @"C:\Hackathon\rsg_hackathon2019_team6\Assets\Areal";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        // Ausgangspunkt für Koordinaten setzen
+        GPSEncoder.SetLocalOrigin(new Vector2(48.32352f, 14.25904f));
+        GameObject lastObject = null;
+        if (encodedGeoJSON != null)
+        {
+            if (GUI.Button(new Rect(0, 0, 200, 200), "Generate Models"))
+            {
+                collection = GeoJSON.GeoJSONObject.Deserialize(encodedGeoJSON.text);
+                foreach (var area in collection.features)
+                //var area = collection.features[0];
+                {
+                    if (area.properties.ContainsKey("path"))
+                    {
+                        var areaJsonPath = AREAL_PATH + "\\" + area.properties["path"];
+                        var areaPath = AREAL_PATH + "\\" + area.properties["id"];
+                        var areaJsonContent = File.ReadAllText(areaJsonPath);
+                        var areas = GeoJSON.GeoJSONObject.Deserialize(areaJsonContent);
+                        foreach (var areaBuildings in areas.features)
+                        {
+                            var coordinates = areaBuildings.properties["origin"].Split(new string[] { "," }, System.StringSplitOptions.RemoveEmptyEntries);
+                            var longitude = float.Parse(coordinates[0]);
+                            var latitude = float.Parse(coordinates[1]);
+
+                            lastObject = new OBJLoader().Load(areaPath + "\\" + areaBuildings.properties["models"].Split(new string[] { "," }, System.StringSplitOptions.RemoveEmptyEntries)[0]);
+                            
+                            var position = GPSEncoder.GPSToUCS(latitude, longitude);
+                            lastObject.transform.position = position;
+                            lastObject.transform.Rotate(0, 180, 0);
+
+                            lastObject.GetComponentInChildren<Renderer>().material.color = Random.ColorHSV(0.2f, 0.4f, 0.4f, 0.5f, 1f, 1f);
+
+                            lastObject.AddComponent<MeshFilter>();
+
+                            var mesh = lastObject.GetComponent<MeshFilter>().mesh;
+                            if (mesh != null)
+                            {
+                                MeshCollider meshCollider = lastObject.AddComponent<MeshCollider>();
+                                meshCollider.sharedMesh = mesh;
+                            }
+                        }
+                    }
+                }
+            }
+            lastObject.SetActive(true);            
+        }
+    }
+}
