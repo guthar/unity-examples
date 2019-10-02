64234e00d2e86e7f3cc6e8659c13b68ca15f5eb4
Harald Guttner <harald.guttner@gmail.com>
2019-10-02 10:57:55 +0200

    White Shark Prefab

---
+++ Assets/Scripts/SharkBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkBehaviour : MonoBehaviour
+{
+    private float currentRotation = 90;
+
+    public float Speed = 2;
+    public float RotationDelta = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentRotation == 360)
+        {
+            currentRotation = 0;
+        }
+
+        currentRotation = currentRotation + RotationDelta * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0, currentRotation, 0);
+        transform.Translate(Speed * Time.deltaTime, 0, 0);
+    }
+}
